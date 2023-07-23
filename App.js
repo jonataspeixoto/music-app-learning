a36bfb7d3391c3b381b0d39b04849a0c11a8fb8e
jonatas.peixoto.zup <jonatas.peixoto@zup.com.br>
2023-07-23 03:04:02 -0300

    App música 1/5

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <StatusBar style="auto" hidden />
+      <View style={styles.header}>
+        <Text style={styles.txtHeader}>Music App</Text>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#222'
   },
+  header: {
+    backgroundColor: '#1DB954',
+    width: '100%',
+    padding: 20
+  },
+  txtHeader: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 25
+  }
 });
